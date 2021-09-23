3553ee42ee1ef7e2cc0355866ccee63e5228ab6d
Philipp Zschoche <git@zschoche.org>
2021-09-23 09:44:45 +0200

    parsing the bib now

--- dblp.py
+++ dblp.py
@@ -1,86 +1,66 @@
+#!/usr/bin/python3
 import urllib.request
 import locale
 import codecs
 import sys
 import json
-from string import digits
+import re
+import dblplib as lib
+import bibtexparser
+from datetime import datetime, timezone
 
-sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
-
-def printLocationDBLP(paper):
-    last = ""
-    if 'pages' in paper:
-        last = ":"
-    print(paper['venue'], end=" ")
-    if 'volume' not in paper:
-        print(paper['year'], end=last)
+def getLocation(paper):
+    if paper["ENTRYTYPE"] == "article":
+        loc = paper["journal"]
+        if loc == "CoRR":
+            loc = "ariXv"
+        return loc + " ("+paper["year"]+")"
     else:
-        print(paper['volume'], end=last)
-
-    if 'pages' in paper:
-        print(" " + paper['pages'], end="")
-    if 'volume' in paper:
-        print(" (" + paper['year'] + ")",end="")
-
-def printLocationShort(paper):
-    if paper['venue'] == 'CoRR':
-        print("arXiv", end=" ")
-    else:
-        print(paper['venue'], end=" ")
-
-    if 'volume' not in paper:
-        print(paper['year'], end="")
-    else:
-        print("(" + paper['year'] + ")",end="")
-
-def lookahead(iterable):
-    """Pass through all values from the given iterable, augmented by the
-    information if there are more values to come after the current one
-    (True), or if it is the last value (False).
-    """
-    # Get an iterator and pull the first value.
-    it = iter(iterable)
-    last = next(it)
-    # Run the iterator to exhaustion (starting from the second value).
-    for val in it:
-        # Report the *previous* value (more to come).
-        yield last, True
-        last = val
-    # Report the last value.
-    yield last, False
-
+        loc = paper["ID"]
+        loc = re.sub('^.*?/', '', loc)
+        loc = re.sub('/.*$', '',  loc).upper()
+        if loc == "IWPEC":
+            loc = "IPEC"
+        if loc == "BIRTHDAY":
+            return  paper["booktitle"] # + " ("+paper["year"]+")"
+        return loc + " " + paper["year"]
 
 def getMyTitleKey(title):
-    title = title.lower()
-    #print(title)
-    #print("<br>")
-    if title == 'the computational complexity of finding separators in temporal graphs.':
-        return "the complexity of finding small separators in temporal graphs."
-    if title == 'facility location under matroid constraints - fixed-parameter algorithms and applications.' or title == 'fixed-parameter algorithms for maximum-profit facility location under matroid constraints.':
-        return "representative families for matroid intersections, with applications to location, packing, and covering problems."
-    if title == 'facility location under matroid constraints - fixed-parameter algorithms and applications.':
-        return "fixed-parameter algorithms for maximum-profit facility location under matroid constraints."
-    if title == 'multistage s-t path - confronting similarity with dissimilarity in temporal graphs.':
-        return "multistage s-t path - confronting similarity with dissimilarity."
-    if title == 'the computational complexity of finding temporal paths under waiting time constraints.':
-        return "finding temporal paths under waiting time constraints."
+    title = title.strip().replace("\n"," ").lower()
+    if title == 'the computational complexity of finding separators in temporal graphs':
+        return "the complexity of finding small separators in temporal graphs"
+    if title == 'facility location under matroid constraints: fixed-parameter algorithms and applications' or title == 'fixed-parameter algorithms for maximum-profit facility location under matroid constraints':
+        return "representative families for matroid intersections, with applications to location, packing, and covering problems"
+    if title == 'facility location under matroid constraints - fixed-parameter algorithms and applications':
+        return "fixed-parameter algorithms for maximum-profit facility location under matroid constraints"
+    if title == 'multistage s-t path: confronting similarity with dissimilarity in temporal graphs':
+        return "multistage s-t path: confronting similarity with dissimilarity"
+    if title == 'the computational complexity of finding temporal paths under waiting time constraints':
+        return "finding temporal paths under waiting time constraints"
     return title    
 
+def getTimestamp(text):
+    #Thu, 18 Feb 2021 00:00:00 +0100
+    return datetime.strptime(text, '%a, %d %b %Y %H:%M:%S %z')
 
-page = urllib.request.urlopen('https://dblp.org/search/publ/api?q=author%3APhilipp%20Zschoche%3A&format=json')
+page = urllib.request.urlopen('https://dblp.org/pid/194/2380.bib?param=1')
 papers = dict()
-result = json.loads(page.read().decode("utf-8"))
-for x in result['result']['hits']['hit']:
-    paper = x['info']
+
+def sortDatetime(val):
+    #return getTimestamp(val['timestamp']).toordinal()
+    return int(val['year'])
+
+
+bib_database = bibtexparser.loads(page.read().decode("utf-8"))
+bib_database.entries.sort(key=sortDatetime, reverse=True)
+
+papers = dict()
+for paper in bib_database.entries:
     title = getMyTitleKey(paper['title'])
     if title in papers:
         papers[title].append(paper)
     else:
         papers[title] = [paper]
-
-
-def sortByYear(val):
-    return int(papers[val][0]["year"])
 
 def sortByYearCount(val):
     last = 0
@@ -93,40 +73,33 @@ def sortByYearCount(val):
         #value += now
     return last*100 + first*10
 
-#(ord(papers[val][0]["title"][0]) -ord('0'))
-
 keys = list(papers.keys())
 keys.sort(key = sortByYearCount, reverse = True)
-#print(keys);
 
-year = '0'
 for key in keys:
     paper = papers[key][0]
-#    if year != paper['year']:
-        #year = paper['year']
-        #print("<h3>" + year + "</h3><hr>")
-    #else:
-    #    print()
-
     print("<p>")
-    if len(paper['authors']['author']) == 2:
-        print(paper['authors']['author']['text'].rstrip(digits+" "), end=":<br>\n")
+    authors = [s.replace("{\\'{e}}", "é").replace("{-}","-").strip() for s in paper["author"].split("and\n")]
+    if len(authors) == 1:
+        print(authors[0], end="")
+    if len(authors) == 2:
+        print(authors[0] + " and " + authors[1], end="")
     else:
-        for author, more in lookahead(paper['authors']['author']):
-            if more:
-                print(author['text'].rstrip(digits+" "), end =", "),
-            elif len(paper['authors']['author']) == 1:
-                print(author['text'].rstrip(digits+" "),":"),
+        for name in authors:
+            if authors[-1] == name:
+                print("and "+name, end="")
             else:
-                print('and', author['text'].rstrip(digits+" "),end=":<br>\n")
-
-    print("<strong>" + paper['title']+ "</strong><br>")
+                print(name+", ", end="")
+    print(":<br>")
+    print("<strong>" + paper['title'].replace("{","").replace("}","")+ "</strong><br>")
     for pub in papers[key]:
-        print("<a href='" + pub["ee"] + "'>")
-        printLocationShort(pub)
+        print("<a href='" + pub["url"] + "'>")
+        print(getLocation(pub))
         print("</a>")
         print("&nbsp;|&nbsp;")
 
-    print("<a href='https://dblp.org/rec/bibtex/" + paper["key"] + "'>BibTeX</a>")
+    print("<a href='https://dblp.org/rec/bibtex/" + paper["biburl"] + "'>BibTeX</a>")
     print("</p>")
 
+
+    print("------------------------------")
